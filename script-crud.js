7af02f278fd01565cda93724cdd4f88feaef710d
marlon-oliveira-wmw <marlon.oliveira@wmw.com.br>
2025-02-03 23:18:29 -0300

    Ajustando o caminho do icone de editar

--- script-crud.js
+++ script-crud.js
@@ -54,7 +54,7 @@ function criarElementoTarefa(tarefa) {
     }
 
     const imagemBotao = document.createElement('img');
-    imagemBotao.setAttribute('src', './imagens/edit.png');
+    imagemBotao.setAttribute('src', 'imagens/edit.png');
     botao.append(imagemBotao);
 
     li.append(svg);
